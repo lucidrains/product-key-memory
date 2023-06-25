70f6af19768ea41e5f3113ffb032643d64167263
Phil Wang <lucidrains@gmail.com>
2023-06-24 20:03:49 -0700

    redo with einops to ready to improve upon

--- product_key_memory/product_key_memory.py
+++ product_key_memory/product_key_memory.py
@@ -1,18 +1,25 @@
-import torch
 import math
-from torch import nn
+import torch
+from torch import nn, einsum
+
+from einops import rearrange
+
+# helper functions
+
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return val if exists(val) else d
+
+# init
 
 def init_(t, dim = None):
-    dim = dim if dim is not None else t.shape[-1]
+    dim = default(dim, t.shape[-1])
     std = 1. / math.sqrt(dim)
     return nn.init.normal_(t, mean=0, std=std)
 
-def expand_dim(t, dim, k, unsqueeze = False):
-    if unsqueeze:
-        t = t.unsqueeze(dim)
-    expand_shape = [-1] * len(t.shape)
-    expand_shape[dim] = k
-    return t.expand(*expand_shape)
+# optimizer
 
 def list_subtract(l, r):
     return [el for el in l if el not in set(r)]
@@ -29,32 +36,42 @@ def fetch_optimizer_parameters(module, pkm_learning_rate = 1e-2):
     pkm_params, rest = fetch_pkm_value_parameters(module)
     return [{'params': rest}, {'params': pkm_params, 'lr': pkm_learning_rate}]
 
+# norm
+
 class MaskedBatchNorm1D(nn.Module):
     def __init__(self, fn):
         super().__init__()
         self.fn = fn
 
-    def forward(self, x, mask = None):
-        b, t, d = x.shape
-        has_mask = mask is not None
-
-        if has_mask:
+    def forward(
+        self,
+        x,
+        mask = None
+    ):
+        if exists(mask):
             initial_x = x
-            mask = mask.unsqueeze(-1)
-            x = x.masked_select(mask)
+            x = x[mask]
 
-        shape = x.shape
-        x = x.reshape(-1, d)
         x = self.fn(x)
-        x = x.reshape(*shape)
 
-        if has_mask:
-            x = initial_x.masked_scatter(mask, x)
+        if exists(mask):
+            initial_x[mask] = x
+            x = initial_x
 
         return x
 
 class PKM(nn.Module):
-    def __init__(self, dim, heads = 4, num_keys = 128, topk = 32, dim_head = 256, input_dropout = 0., query_dropout = 0., value_dropout = 0.):
+    def __init__(
+        self,
+        dim,
+        heads = 4,
+        num_keys = 128,
+        topk = 32,
+        dim_head = 256,
+        input_dropout = 0.,
+        query_dropout = 0.,
+        value_dropout = 0.
+    ):
         super().__init__()
         assert (dim % heads == 0), 'dimension must be divisible by number of heads'
         self.topk = topk
@@ -74,42 +91,46 @@ class PKM(nn.Module):
         self.query_dropout = nn.Dropout(query_dropout)
         self.value_dropout = nn.Dropout(value_dropout)
 
-    def forward(self, x, input_mask = None, **kwargs):
-        b, t, e, h = *x.shape, self.heads
+    def forward(
+        self,
+        x,
+        input_mask = None,
+        **kwargs
+    ):
+        b, t, h = *x.shape[:2], self.heads
         x = self.input_dropout(x)
 
         queries = self.to_queries(x)
         queries = self.norm(queries, mask = input_mask)
         queries = self.query_dropout(queries)
 
-        queries = queries.chunk(2, dim=-1)
-        queries = torch.stack(queries).reshape(2, b, t, h, -1)
+        queries = rearrange(queries, 'b t (p h d) -> p b t h d', p = 2, h = h)
 
-        dots = torch.einsum('pbthd,hnpd->bthpn', queries, self.keys)
-        scores, indices = dots.topk(k=self.topk, dim=-1)
-        scores, indices = map(lambda x: x.chunk(2, dim=3), (scores, indices))
+        dots = einsum('p b t h d, h n p d -> b t h p n', queries, self.keys)
+        scores, indices = dots.topk(k = self.topk, dim = -1)
+
+        (scores_x, scores_y), (indices_x, indices_y) = map(lambda t: t.chunk(2, dim = 3), (scores, indices))
 
         all_topk = self.topk ** 2
-        shape = (b, t, h, all_topk)
 
-        all_scores = (
-            scores[0][..., :, None] +
-            scores[1][..., None, :]
-        ).reshape(*shape)
+        all_scores = rearrange((
+            rearrange(scores_x, '... k -> ... k 1') +
+            rearrange(scores_y, '... k -> ... 1 k')
+        ), 'b t h ... -> b t h (...)')
 
-        all_indices = (
-            indices[0][..., :, None] * self.num_keys +
-            indices[1][..., None, :]
-        ).reshape(*shape)
+        all_indices = rearrange((
+            rearrange(indices_x, '... k -> ... k 1') * self.num_keys +
+            rearrange(indices_y, '... k -> ... 1 k')
+        ), 'b t h ... -> b t h (...)')
 
         final_topk, final_indices = all_scores.topk(self.topk, dim=-1)
         value_indices = all_indices.gather(-1, final_indices)
 
         attn = final_topk.softmax(dim=-1)
 
-        value_indices, attn = map(lambda x: x.reshape(-1, self.topk * h), (value_indices, attn))
+        value_indices, attn = map(lambda t: rearrange(t, 'b t h k -> (b t) (h k)'), (value_indices, attn))
 
         out = self.values(value_indices, per_sample_weights=attn)
         out = self.value_dropout(out)
-        return out.reshape(b, t, e)
 
+        return rearrange(out, '(b t) d -> b t d', b = b)
--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'product_key_memory',
-  packages = find_packages(),
-  version = '0.1.10',
-  license='MIT',
-  description = 'Product Key Memory',
-  author = 'Aran Komatsuzaki, Phil Wang',
-  author_email = 'aran1234321@gmail.com, lucidrains@gmail.com',
-  url = 'https://github.com/lucidrains/product-key-memory',
-  keywords = ['transformers', 'artificial intelligence'],
-  install_requires=[
-      'torch'
-  ],
-  classifiers=[
-      'Development Status :: 4 - Beta',
-      'Intended Audience :: Developers',
-      'Topic :: Scientific/Engineering :: Artificial Intelligence',
-      'License :: OSI Approved :: MIT License',
-      'Programming Language :: Python :: 3.6',
-  ],
+    name = 'product_key_memory',
+    packages = find_packages(),
+    version = '0.1.11',
+    license='MIT',
+    description = 'Product Key Memory',
+    long_description_content_type = 'text/markdown',
+    author = 'Aran Komatsuzaki, Phil Wang',
+    author_email = 'aran1234321@gmail.com, lucidrains@gmail.com',
+    url = 'https://github.com/lucidrains/product-key-memory',
+    keywords = ['transformers', 'artificial intelligence'],
+    install_requires=[
+        'einops>=0.6',
+        'torch'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
