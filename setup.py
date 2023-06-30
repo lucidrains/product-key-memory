3b1db4a2fde441a8b126b5b300c5a5e827ac1d3c
Phil Wang <lucidrains@gmail.com>
2023-06-30 14:48:47 -0700

    0.2.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'product_key_memory',
     packages = find_packages(),
-    version = '0.2.7',
+    version = '0.2.8',
     license = 'MIT',
     description = 'Product Key Memory',
     long_description_content_type = 'text/markdown',
